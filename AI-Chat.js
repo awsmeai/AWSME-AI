0c2929d8dc38485a978a7ee5bade9788e5cd9723
Kevin Fremon <kevin@awsme.ai>
2024-02-29 15:37:19 -0600

    remove awsme branding
    Removed the AWSME branding for the paid accounts until we can make it smarter.

--- AI-Chat.js
+++ AI-Chat.js
@@ -205,9 +205,6 @@ const chatHTML = `<div class="awsme-ai-chat fade-in" style="z-index:1000000; pos
           <textarea class="awsme-user-input" type="text" placeholder="${chatPlaceholder}"></textarea>
           <div class="awsme-send-icon"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M3.4 78.3c-6-12-3.9-26.5 5.3-36.3s23.5-12.7 35.9-7.5l448 192c11.8 5 19.4 16.6 19.4 29.4s-7.6 24.4-19.4 29.4l-448 192c-12.3 5.3-26.7 2.3-35.9-7.5s-11.3-24.3-5.3-36.3L92.2 256 3.4 78.3zM120 272L32 448 442.7 272H120zm322.7-32L32 64l88 176H442.7z"/></svg></i></div>
         </div>
-        <p class="awsme-input-note">
-          <a href="https://awsme.ai" target="_blank">AWSME.ai</a> - Always on. Always AWSME.
-        </p>
       </div>
     </div>
   </div>
