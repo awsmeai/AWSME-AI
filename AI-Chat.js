e856265c7db9a867ad0a273e7e5eaf11e239410b
LeoMcMillion <148974398+LeoMcMillion@users.noreply.github.com>
2024-03-29 22:17:59 +0100

    Update AI-Chat.js

--- AI-Chat.js
+++ AI-Chat.js
@@ -94,7 +94,10 @@ var dynamicAddedCSS = `.awsme-sidebar {
     .awsme-ai-chat .awsme-trigger {
       background-color: ${triggerBgCol};
       ${triggerPosCSS}
-    }      
+    } 
+    .awsme-hide-trigger-btn {
+      ${triggerPosCSS}
+    }
     .awsme-ai-chat .awsme-trigger p {
       color: ${triggerTextCol};
     }
@@ -123,7 +126,7 @@ var dynamicAddedCSS = `.awsme-sidebar {
     .awsme-close-x svg:hover {
       fill: ${closeHoverCol};
     }
-    .awsme-ai-chat .awsme-bot .awsme-profile {
+    .awsme-ai-chat .awsme-bot .awsme-profile, .awsme-bot-image {
       background-image: url(${botImgUrl});
     }
     .awsme-ai-chat .awsme-user .awsme-profile {
@@ -195,13 +198,19 @@ newStyleTag.textContent = dynamicAddedCSS;
 document.head.appendChild(newStyleTag);
 
  
-const chatHTML = `<div class="awsme-ai-chat fade-in" style="z-index:1000000; position: relative;">
-  <div class="awsme-trigger">
+const chatHTML = `<div class="awsme-ai-chat awsme-fade-in" style="z-index:1000000; position: relative;">
+    <div class="awsme-hide-trigger-btn">
+      <div class="awsme-hide-trigger-btn-arrows"></div>
+    </div>
+    <div class="awsme-trigger">
       <span class="awsme-wave">👋</span> 
-    <p style="margin-bottom: 0px;">
-      ${triggerLabel}
-    </p>
-  </div>
+      <div class="awsme-inner-trigger">
+        <div class="awsme-profile awsme-bot-image" style="margin-right:10px; border:1px solid white; width:30px; height:30px;"></div>
+        <p style="margin-bottom: 0px;">
+          ${triggerLabel}
+        </p>
+      </div>
+    </div>
   <div class="awsme-sidebar">
     <div class="awsme-top-area">
       <div class="awsme-close-x"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><path d="M324.5 411.1c6.2 6.2 16.4 6.2 22.6 0s6.2-16.4 0-22.6L214.6 256 347.1 123.5c6.2-6.2 6.2-16.4 0-22.6s-16.4-6.2-22.6 0L192 233.4 59.5 100.9c-6.2-6.2-16.4-6.2-22.6 0s-6.2 16.4 0 22.6L169.4 256 36.9 388.5c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0L192 278.6 324.5 411.1z"/></svg></div>
@@ -274,9 +283,11 @@ const chatHTML = `<div class="awsme-ai-chat fade-in" style="z-index:1000000; pos
   
   chatWidth = screen.width > chatWidth ? chatWidth:screen.width;
   const triggerButton = document.querySelector('.awsme-trigger');
+  const hideTriggerButton = document.querySelector('.awsme-hide-trigger-btn');
   const sendButton = document.querySelector('.awsme-send-icon');
   const closeButton = document.querySelector('.awsme-close-x');
   const sidebar = document.querySelector('.awsme-sidebar');
+  const triggerWidth = triggerButton.offsetWidth;
 
   const reviewIcons = ['<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M288.8 81.7c3.5-12.8 16.7-20.3 29.5-16.8s20.3 16.7 16.8 29.5l-4.5 16.4c-5.5 20.2-13.9 39.3-24.7 56.9c-3.1 4.9-3.2 11.1-.4 16.2s8.2 8.2 14 8.2H448c17.7 0 32 14.3 32 32c0 11.3-5.9 21.3-14.8 27c-7.2 4.6-9.5 13.9-5.3 21.3c2.6 4.6 4.1 10 4.1 15.7c0 12.4-7 23.1-17.3 28.5c-4.2 2.2-7.3 6.1-8.3 10.8s.1 9.5 3 13.2c4.2 5.4 6.7 12.2 6.7 19.5c0 14.2-9.2 26.3-22.1 30.4c-7.8 2.5-12.4 10.6-10.7 18.6c.5 2.2 .7 4.5 .7 6.9c0 17.7-14.3 32-32 32H294.5c-15.8 0-31.2-4.7-44.4-13.4l-38.5-25.7c-9-6-16.6-13.7-22.4-22.6c-4.9-7.4-14.8-9.4-22.2-4.6s-9.4 14.8-4.6 22.2c8.1 12.3 18.7 23.1 31.4 31.6l38.5 25.7c18.4 12.3 40 18.8 62.1 18.8H384c35.3 0 64-28.7 64-64l0-.6c19.1-11.1 32-31.7 32-55.4c0-8.7-1.8-17.1-4.9-24.7C487.9 323.6 496 306.8 496 288c0-6.5-1-12.8-2.8-18.7C504.8 257.7 512 241.7 512 224c0-35.3-28.7-64-64-64H346.4c6.2-13.1 11.3-26.7 15.1-40.9l4.5-16.4c8.1-29.8-9.5-60.6-39.3-68.8s-60.6 9.5-68.8 39.3l-4.5 16.4c-8.9 32.6-29.6 60.8-58.2 79l-3.1 2c-11.8 7.5-21.7 17.1-29.5 28.2c-5.1 7.2-3.3 17.2 4 22.3s17.2 3.3 22.3-4c5.4-7.7 12.2-14.4 20.4-19.5l3.1-2c35.3-22.4 60.9-57.2 71.9-97.5l4.5-16.4zM32 224H96V448H32V224zM0 224V448c0 17.7 14.3 32 32 32H96c17.7 0 32-14.3 32-32V224c0-17.7-14.3-32-32-32H32c-17.7 0-32 14.3-32 32z"/></svg>',
                       '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M480 256A224 224 0 1 1 32 256a224 224 0 1 1 448 0zM256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM176.4 232a24 24 0 1 0 0-48 24 24 0 1 0 0 48zm184-24a24 24 0 1 0 -48 0 24 24 0 1 0 48 0zM176 336c-8.8 0-16 7.2-16 16s7.2 16 16 16H336c8.8 0 16-7.2 16-16s-7.2-16-16-16H176z"/></svg>',
@@ -313,6 +324,7 @@ const chatHTML = `<div class="awsme-ai-chat fade-in" style="z-index:1000000; pos
     let userEmail = localStorage.getItem('email_'+awsmeId) != null ? localStorage.getItem('email_'+awsmeId): "";
     let isLead = localStorage.getItem('isLead_'+awsmeId) == "true";
     let openChat = getWithExpiry('open_chat_'+awsmeId) != null ? getWithExpiry('open_chat_'+awsmeId):true;
+    let openTrigger = getWithExpiry('open_trigger_'+awsmeId) != null ? getWithExpiry('open_trigger_'+awsmeId):true;
     let usedInteractions = [];
     let firstClick = true;
     
@@ -348,18 +360,35 @@ const chatHTML = `<div class="awsme-ai-chat fade-in" style="z-index:1000000; pos
         setWithExpiry('open_chat_'+awsmeId, false, 3600000)
       }
     }
+    function showTrigger() {
+      triggerButton.style.right = 0;
+      hideTriggerButton.style.display = "block";
+      setWithExpiry('open_trigger_'+awsmeId, true, 3600000)
+    }
+    function hideTrigger() {
+      triggerButton.style.right = (64-triggerWidth) + "px";
+      hideTriggerButton.style.display = "none";
+      setWithExpiry('open_trigger_'+awsmeId, false, 3600000)
+    }
     
     if (autoOpen && openChat) {
       openSidebar();
     }
+    if (!openTrigger) {
+      hideTrigger();
+    }
     
     triggerButton.addEventListener('click', () => {
       openSidebar();
+      showTrigger();
     });
     closeButton.addEventListener('click', () => {
-      closeSidebar()
+      closeSidebar();
     });
-
+    
+    hideTriggerButton.addEventListener('click', () => {
+      hideTrigger();
+    });
 
     function postmark_send_email_template(templateId, templateModel) {
       let response = fetch('https://awsme.co/api/send-email/', {
@@ -1045,7 +1074,7 @@ const chatHTML = `<div class="awsme-ai-chat fade-in" style="z-index:1000000; pos
     AITextGen(responseDiv, AIFirstGreeting)
 
     updateMetric("numTriggerViews");
-  }, 200)
+  }, 0)
 
   document.addEventListener('DOMContentLoaded', function() {
     var textarea = document.querySelector('textarea.awsme-user-input');
